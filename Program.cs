563117a3bfd403c0aea64e623d98365b91657273
M4Barboza <matheus.barboza0@gmail.com>
2025-07-29 18:35:56 -0300

    Make the first migration with EF

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryStore.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(connectionString);
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
