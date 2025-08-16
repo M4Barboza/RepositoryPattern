7ad71d488897873a58e49c66f5d387cd894db335
M4Barboza <matheus.barboza0@gmail.com>
2025-08-16 13:49:29 -0300

    Make the interface and the ProductRepository

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryStore.Data;
+using RepositoryStore.Repositories;
+using RepositoryStore.Repositories.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,8 +11,14 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(connectionString);
 });
 
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/v1/products", async (IProductRepository repository) => await repository.GetAllAsync(0,30));
+
+app.MapPost("/v1/products", (AppDbContext context) => "Hello World!");
+app.MapPut("/v1/products", (AppDbContext context) => "Hello World!");
+app.MapDelete("/v1/products", (AppDbContext context) => "Hello World!");
 
 app.Run();
